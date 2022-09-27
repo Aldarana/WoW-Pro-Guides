e1b47986b35c295412f47bd8b72207e89b8d3ab2
jamiejme09 <113785394+jamiejme09@users.noreply.github.com>
2022-09-27 23:09:20 +0100

    Added The Azure Span

---
+++ WoWPro_Leveling/Neutral/DF_The_Azure_Span.lua
@@ -0,0 +1,11 @@
+local guide = WoWPro:RegisterGuide("DF_AS", "Leveling", "The Azure Span", "Aldarana and Jme", "Neutral", 10)
+WoWPro:GuideName(guide,"The Azure Span")
+WoWPro:GuideContent(guide, "Dragonflight")
+WoWPro:GuideLevels(guide, 65, 68)
+WoWPro:GuideSort(guide, 3)
+WoWPro:GuideSteps(guide, function()
+return [[
+  ;Chapter 1
+
+  ]]
+end)

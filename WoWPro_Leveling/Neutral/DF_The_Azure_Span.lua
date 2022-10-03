1d4348b55f66624e3580f4907522d4d204e42fe4
jamiejme09 <113785394+jamiejme09@users.noreply.github.com>
2022-10-03 12:22:55 +0100

    Fixed some quest coords and Note's

--- WoWPro_Leveling/Neutral/DF_The_Azure_Span.lua
+++ WoWPro_Leveling/Neutral/DF_The_Azure_Span.lua
@@ -6,7 +6,7 @@ WoWPro:GuideSort(guide, 3)
 WoWPro:GuideSteps(guide, function()
 return [[
   ;Chapter 1
-   A Ca mp Antonidas|QID|66228|M|41.38,35.70|N|From Giania of the Blessed Ones.
+   A Camp Antonidas|QID|66228|M|41.38,35.70|N|From Giania of the Blessed Ones.
    A Arcane Detection|QID|67174|M|41.22,35.88|N|From Miva Star-Eye.
    C Arcane Detection|QID|67174|QO|1|M|41.38,35.70|N|
    C Arcane Detection|QID|67174|QO|2|M|41.41,35.83|N|
@@ -32,6 +32,7 @@ return [[
    T Wrath of the Kirin Tor|QID|67036|M|46.64,40.16|N|To Noriko The All-remembering
    A WANTED: Frigellus|QID|66488|M|46.16,39.61|N|The Wanted Poster
    A Meeting Kalecgos|QID|65688|M|46.64,40.16|N|From Khadgar
+   A Path to Nowhere|QID|66671|M|45.68,39.78|N|To Maddy Scattershot 
    C Meeting Kalecgos|QID|65688|M|45.99,38.80|N|Click portal, Then a bag will appear on the floor next to you.
    C WANTED: Frigellus|QID|66488|M|47.93,38.05|N|Kill Frigellus
    T WANTED: Frigellus|QID|66488|M|46.01,38.31|N|To Custodian Cynegos
@@ -44,11 +45,12 @@ return [[
    T The Many Images of Kalecgos|QID|65689|M|40.75,59.03|N|To Kalecgos
    A Driven Mad|QID|65702|M|40.75,59.03|N|From Kalecgos
    A Arcane Pruning|QID|65709|M|40.69,59.11|N|From Kalecgos
+   
    C Driven Mad|QID|65702|M|41.18,62.25|N|Kill and loot mobs|S|
-   C Driven Mad|QID|65702|M|41.18,62.25|N|Kill and loot mobs|US|
    C Arcane Pruning|QID|65709|QO|1|M|40.69,59.11|N|Use item "Bag of Helpful Goods"
    C Arcane Pruning|QID|65709|QO|2|M|39.57,60.29|N|North arcane field repaired
    C Arcane Pruning|QID|65709|QO|3|M|41.18,62.25|N|East arcane field repaired
+   C Driven Mad|QID|65702|M|41.18,62.25|N|Kill and loot mobs|US|
    T Driven Mad|QID|65702|M|40.75,59.03|N|To Kalecgos
    T Arcane Pruning|QID|65709|M|40.75,59.03|N|To Kalecgos
    A Straight to the Top|QID|65852|M|40.75,59.03|N|From Kalecgos
@@ -65,12 +67,14 @@ return [[
    C Reclaiming the Oathstone|QID|65854|M|39.44,63.18|N|Fly down to Kalecgos
    T Reclaiming the Oathstone|QID|65854|M|39.49,63.07|N|To Sindragosa
    A Aiding Azure Span|QID|65855|M|39.49,63.07|N|From Sindragosa
+   h The Conjured Biscuit|QID|65855|M|37.37,62.42|N|At the innkeeper.|S|
    C Aiding Azure Span|QID|65855|M|37.06,60.82|N|Speak to the Flightmaster
+   h The Conjured Biscuit|QID|65855|M|37.37,62.42|N|At the innkeeper.
    T Aiding Azure Span|QID|65855|M|46.64,40.16|N|To Noriko The All-remembering
 
   ;Chapter 2
-   A Ask the Locals|QID|66699|M|46.77,38.58|N|Add Note
-   A Suspiciously Quiet|QID|69904|M|99.99,99.99|N|Add Note
+   A Ask the Locals|QID|66699|M|46.63,40.16|N|From Noriko The All-remembering
+   A Suspiciously Quiet|QID|69904|M|46.63,40.16|N|From Noriko The All-remembering
    C Ask the Locals|QID|66699|QO|1|M|46.77,38.58|N|Speak to korrikunit the Whalebringer
    C Ask the Locals|QID|66699|QO|2|M|46.33,38.15|N|Speak to Jokomuupat
    C Ask the Locals|QID|66699|QO|3|M|45.72,38.85|N|Speak to Babunituk
@@ -81,13 +85,13 @@ return [[
    A Those Aren't for Chewing|QID|65868|M|35.37,36.94|N|From Elder Poa
    A Howling in the Big Tree Hills|QID|65867|M|35.25,36.98|N|From Brena
    A Snap the Traps|QID|65866|M|35.25,36.98|N|From Brena
-   C Those Aren't for Chewing|QID|65868|M|34.82,33.79|N|Add Note|S|
-   C Snap the Traps|QID|65866|M|34.82,33.79|N|Add Note|S|
+   C Those Aren't for Chewing|QID|65868|M|34.82,33.79|S|
+   C Snap the Traps|QID|65866|M|34.82,33.79|S|
    C Howling in the Big Tree Hills|QID|65867|QO|1|M|35.62,34.66|N|Kill Hyenamaster Durgun
    C Howling in the Big Tree Hills|QID|65867|QO|3|M|34.01,33.22|N|Kill Bakra the Bully
    C Howling in the Big Tree Hills|QID|65867|QO|2|M|34.98,32.20|N|kill Trap-layer Kerwal, He's in the cave
-   C Those Aren't for Chewing|QID|65868|M|34.82,33.79|N|Add Note|US|
-   C Snap the Traps|QID|65866|M|34.82,33.79|N|Add Note|US|
+   C Those Aren't for Chewing|QID|65868|M|34.82,33.79|US|
+   C Snap the Traps|QID|65866|M|34.82,33.79|N|US|
    T Those Aren't for Chewing|QID|65868|M|34.27,31.30|N|To Hanu
    T Howling in the Big Tree Hills|QID|65867|M|34.27,31.30|N|To Hanu
    T Snap the Traps|QID|65866|M|34.27,31.30|N|To Hanu
@@ -109,8 +113,8 @@ return [[
    T Ill Gnolls With Ill Intent|QID|65872|M|34.36,31.07|N|To Brena
    T Leader of the Shadepaw Pack|QID|65873|M|34.36,31.07|N|To Brena
    A Spreading Decay|QID|66239|M|34.36,31.07|N|From Brena
-   T Spreading Decay|QID|66239|M|26.68,34.82|N|To Brena
-   A Another Ambush|QID|65869|M|26.68,34.82|N|From Brena
+   T Spreading Decay|QID|66239|M|28.68,34.82|N|To Brena
+   A Another Ambush|QID|65869|M|28.68,34.82|N|From Brena
    A Falling Water|QID|71233|M|28.47,35.12|N|From Unkimi
    C Another Ambush|QID|65869|QO|1|M|28.68,34.76|N|Speak to Akiun
    C Another Ambush|QID|65869|QO|2|CS|M|29.25,33.57;29.17,32.59;28.19,31.86;27.96,32.81|N|Click on the bodies
@@ -139,19 +143,19 @@ return [[
    T Nothing for Breakfast|QID|66837|M|18.74,24.47|N|To Willa Stronghinge
    T It's Cold Up Here|QID|66838|M|18.74,24.47|N|To Willa Stronghinge
    A Water Safety|QID|66840|M|18.54,23.67|N|From Manny Read
-   A A Shard of the Past|QID|66842|M|19.15,24.72|N|Gannar Fullpack
+   A A Shard of the Past|QID|66841|M|19.15,24.72|N|Gannar Fullpack
    C Water Safety|QID|66840|CS|M|19.39,23.94;20.01,24.18;17.31,23.19|N|Use Quest item
-   C A Shard of the Past|QID|66842|M|17.61,28.23|N|Click on the Artifact
+   C A Shard of the Past|QID|66841|M|17.61,28.23|N|Click on the Artifact
    T Water Safety|QID|66840|M|18.54,23.67|N|To Manny Read
-   T A Shard of the Past|QID|66842|M|18.65,23.68|N|To Thanessa
+   T A Shard of the Past|QID|66841|M|18.65,23.68|N|To Thanessa
    A Legendary Foil|QID|66845|M|18.75,24.42|N|From Guo-hee
    C Legendary Foil|QID|66845|M|17.29,26.15|N|Kill Apparition
    T Legendary Foil|QID|66845|M|18.75,24.42|N|To Guo-hee
    A The Heart of the Deck|QID|66846|M|18.75,24.42|N|From Apparition
    C The Heart of the Deck|QID|66846|M|18.75,24.42|N|Speak to Apparition
    T The Heart of the Deck|QID|66846|M|18.75,24.42|N|To Apparition
-   C Breaching the Brackenhide|QID|65838|QO|1|M|18.37,34.74|N|Click the Root
    A Stop the Spread|QID|65841|M|17.68,38.09|N|Add Note
+   C Breaching the Brackenhide|QID|65838|QO|1|M|18.37,34.74|N|Click the Root
    C Stop the Spread|QID|65841|M|17.68,38.09|N|Add Note|S|
    C Breaching the Brackenhide|QID|65838|QO|2|M|18.90,37.00|N|Click the Root
    C Breaching the Brackenhide|QID|65838|QO|3|M|17.59,37.02|N|Click the Root
@@ -159,9 +163,10 @@ return [[
    A Echoes of the Fallen|QID|65845|M|16.74,37.27|N|From Brena
    A Cut Out the Rot|QID|65844|M|16.74,37.27|N|From Norukk
    A Ley Litter|QID|65846|M|16.74,37.27|N|From Kalecgos
-   C Echoes of the Fallen|QID|65845|M|17.68,38.09|N|Use Quest item on the bodies
+   C Echoes of the Fallen|QID|65845|M|17.68,38.09|N|Use Quest item on the bodies|S|
    C Cut Out the Rot|QID|65844|M|17.68,38.09|S|
    C Ley Litter|QID|65846|M|17.68,38.09|S|
+   C Echoes of the Fallen|QID|65845|M|17.68,38.09|N|Use Quest item on the bodies|US|
    C Cut Out the Rot|QID|65844|M|17.68,38.09|US|
    C Ley Litter|QID|65846|M|17.68,38.09|US|
    T Echoes of the Fallen|QID|65845|M|16.74,37.27|N|To Brena
@@ -179,34 +184,35 @@ return [[
    T To Iskaara|QID|65849|M|13.24,49.52|N|To Brena
    A Gather the Family|QID|66210|M|13.24,49.52|N|From Brena
    C Gather the Family|QID|66210|QO|1|CS|M|13.86,49.47;12.47,50.32;12.48,49.44;13.48,48.44|N|Click Offerings
-   C Gather the Family|QID|66210|QO|2|M|13.13,48.60|N|Add Note
+   C Gather the Family|QID|66210|QO|2|M|13.13,48.60|N|Click on the boat
    A The Weave of a Tale|QID|66210|M|12.42,49.39|N|Fromm Nappa
    C The Weave of a Tale|QID|66210|M|12.42,49.39|N|Speak to Poa
    T The Weave of a Tale|QID|66210|M|12.42,49.39|N|Too Nappa
    A WANTED: Krojek the Shoreprowler|QID|66217|M|13.21,48.81|N|From bukarakikk
    A Scampering Scamps|QID|66218|M|12.91,48.70|N|From Hanu
+   T Gather the Family|QID|66210|M|13.24,49.52|N|To Brena
    A Rowie|QID|66558|M|13.76,47.63|N|From Auntie Kaunnie
    C Rowie|QID|66558|M|16.12,50.43|N|Speak to Rowie
    T Rowie|QID|66558|M|16.12,50.43|N|To Rowie
    A Toejam the Terrible|QID|70129|M|16.12,50.43|N|From Rowie
    C Toejam the Terrible|QID|70129|M|16.77,49.36|N|Speak to Toejam
    T Toejam the Terrible|QID|70129|M|13.80,49.01|N|To Auntie Kaunnie
-   T Gather the Family|QID|66210|M|13.24,49.52|N|To Brena
    A The Cycle of the Sea|QID|65850|M|13.24,49.52|N|From Kalecgos
    C The Cycle of the Sea|QID|65850|QO|1|M|13.24,49.52|N|Speak to Breana
    C The Cycle of the Sea|QID|65850|QO|2|M|13.16,50.14|N|Push the boat
    T The Cycle of the Sea|QID|65850|M|12.94,50.39|N|To Kalecgos
    A Azure Alignment|QID|65911|M|12.94,50.39|N|From Kalecgos
-   C Scampering Scamps|QID|66218|M|99.99,99.99|N|Add Note
-   T Scampering Scamps|QID|66218|M|99.99,99.99|N|Add Note
+   C Scampering Scamps|QID|66218|M|10.58,46.85|N|Speak to Neelo
+   T Scampering Scamps|QID|66218|M|10.58,46.85|N|To Neelo
    A Can We Keep It?|QID|66223|M|10.59,46.87|N|From Neelo
    C Can We Keep It?|QID|66223|M|13.85,49.49|N|Buy 7 Frigidfish from the vendor
    T Can We Keep It?|QID|66223|M|10.59,46.87|N|To Neelo
    C WANTED: Krojek the Shoreprowler|QID|66217|M|10.70,54.38|N|Skip if you think its to hard
-   T WANTED: Krojek the Shoreprowler|QID|66217|M|99.99,99.99|N|Skip if it was to hard
-   H  Maruukai |QID|66340|U|6948|
+   T WANTED: Krojek the Shoreprowler|QID|66217|M|12.51,49.80|N|Skip if it was to hard
+   H The Conjured Biscuit|QID|65911|U|6948||S|
    T Azure Alignment|QID|65911|M|39.47,63.02|N|To kalecgos
- 
+   H The Conjured Biscuit|QID|65911|U|6948||US|
+
   ;Chapter 4
    A Calling the Blue Dragons|QID|66027|M|39.47,63.02|N|From Kalecgos
    C Calling the Blue Dragons|QID|66027|M|39.47,63.02|N|Speak to Kalecgos
@@ -237,7 +243,7 @@ return [[
    A Primal Power|QID|65958|M|74.46,37.82|N|From Khadgar
    C Primal Power|QID|65958|M|75.56,38.29|N|Use Quest item to recover Energies|S|
    C Kirin Tor Recovery|QID|65977|M|75.56,38.29|N|Click on the Elemental Drainer's
-   C Primal Power|QID|65958|M|75.56,38.29|N|Use Quest item to recover EnergiesUS|
+   C Primal Power|QID|65958|M|75.56,38.29|N|Use Quest item to recover Energies|US|
    T Kirin Tor Recovery|QID|65977|M|76.94,39.43|N|To Khadgar
    T Primal Power|QID|65958|M|76.94,39.43|N|To Khadgar
    A Free Air|QID|66007|M|76.94,39.43|N|From Khadgar  
@@ -285,11 +291,11 @@ return [[
    T kill it with fire|QID|70856|M|67.23,44.39|N|From To'tik
    T The fending Flames|QID|67724|M|67.23,44.39|N|From To'tik
    A Back into the Action|QID|70858|M|67.23,44.39|N|From Kattigat
-   C Back into the Action|QID|70858|M|67.23,44.39|N|Click on Alia Sunsoar, she on the floor
+   C Back into the Action|QID|70858|M|67.23,44.39|N|Click on Alia Sunsoar, she is on the floor
    T Back into the Action|QID|70858|M|67.23,44.39|N|To Alia Sunsoar
-   A What the Guardian Beholds|QID|70858|M|67.23,44.39|N|From Alia Sunsoar
-   A What the Guardian Beholds|QID|70858|M|69.53,43.25|N|Fly to the top of the tower, and kill Edavis
-   T What the Guardian Beholds|QID|70858|M|67.23,44.39|N|To Alia Sunsoar
+   A What the Guardian Beholds|QID|70859|M|67.23,44.39|N|From Alia Sunsoar
+   C What the Guardian Beholds|QID|70859|M|69.53,43.25|N|Fly to the top of the tower, and kill Edavis
+   T What the Guardian Beholds|QID|70859|M|67.23,44.39|N|To Alia Sunsoar
    A Whispered Fragments|QID|70931|M|67.23,44.39|N|From Alia Sunsoar
    A What Valthrux Once Was|QID|70937|M|67.23,44.39|N|From To'tik
    C Whispered Fragments|QID|70931|M|67.23,44.39|N|From Alia Sunsoar|S|
@@ -303,7 +309,7 @@ return [[
    C No One May Wield It|QID|70946|M|67.23,44.39|N|Speak to Alia Sunsoar
    T No One May Wield It|QID|70946|M|67.23,44.39|N|From To'tik
    A Good Intentions|QID|70970|M|67.23,44.39|N|From Kattigat
-   C Good Intentions|QID|70970|QO|1|CS|M|71.67,43.74;72.25,45.91|N|Fly to marker and Kill Alia Sunsoar
+   C Good Intentions|QID|70970|QO|1|CS|M|71.67,43.74;72.25,42.91|N|Fly to marker and Kill Alia Sunsoar
    C Good Intentions|QID|70970|QO|2||M|72.56,42.86|N|Click the book to Destroy it
    T Good Intentions|QID|70970|M|67.23,44.39|N|To Kattigat
    T Path to Nowhere|QID|66671|M|63.22,58.40|N|To Maddy Scattershot
@@ -347,37 +353,38 @@ return [[
    C Rescuing Radza|QID|66150|QO|2|M|60.98,51.40|N|From Garz
    C Former Furbolg Family|QID|66148|M|61.77,51.75|N|From Garz|US|
    C Elemental Influence|QID|66149|M|61.77,51.75|N|From Garz|US|
-   T Former Furbolg Family|QID|66148|M|63.50,52.98|N|To Narn
-   T Elemental Influence|QID|66149|M|63.50,52.98|N|To Narn
-   T Rescuing Radza|QID|66150|M|63.50,52.98|N|To Radza Thunderclaw
+   T Former Furbolg Family|QID|66148|M|60.93,50.53|N|To Narn
+   T Elemental Influence|QID|66149|M|60.93,50.53|N|To Narn
+   T Rescuing Radza|QID|66150|M|60.93,50.53|N|To Radza Thunderclaw
    A His Stone Heart|QID|66151|M|60.91,50.50|N|From Radza Thunderclaw
-   C His Stone Heart|QID|66151|M|63.50,52.98|N|Kill Stoneheart
+   C His Stone Heart|QID|66151|M|60.60,49.63|N|Kill Stoneheart
    T His Stone Heart|QID|66151|M|60.91,50.50|N|To Radza Thunderclaw 
    T Nowhere to Go|QID|66152|M|63.19,58.65|N|To Garz
-   A I Will Remember|QID|70627|M|63.69,58.92|N|From Rannan Korren
+   A I Will Remember|QID|66429|M|63.69,58.92|N|From Rannan Korren
    A What of the Winterpelt Clan?|QID|66429|M|63.69,58.92|N|To Rannan Korren
    H The Conjured Biscuit|QID|66015|U|6948||S|
    C The Blue Dragon Oathstone|QID|66015|M|39.49,63.06|N|Speak to Sindragosa
    T The Blue Dragon Oathstone|QID|66015|M|39.49,63.06|N|To Sindragosa
    A To Valdrakken|QID|66244|M|39.49,63.06|N|From Sindragosa
-   T I Will Remember|QID|70627|M|63.69,58.92|N|To Sonova Snowden
+   T I Will Remember|QID|66429|M|63.69,58.92|N|To Sonova Snowden
    A Aggressive Self-Defence|QID|66554|M|65.38,15.93|N|From Sonova Snowden
    A Wayward Tools|QID|66622|M|65.57,16.17|N|From Tyrnokos Sunstrike
    A Hollow Up|QID|66244|M|65.38,15.93|N|From Sonova Snowden
    C Wayward Tools|QID|66622|M|65.55,17.18|N|Click the tools in the area
    T Wayward Tools|QID|66622|M|65.57,16.17|N|To Tyrnokos Sunstrike
-   C Aggressive Self-Defence|QID|66554|M|65.38,15.93|N|From Sonova Snowden|S|
+   C Aggressive Self-Defence|QID|66554|M|66.23,12.77|N|From Sonova Snowden|S|
    ;C Hollow Up|QID|66244|M|39.49,63.06|N|From Sonova Snowden|S|
-   C Aggressive Self-Defence|QID|66554|M|65.38,15.93|N|From Sonova Snowden|US|
+   C Aggressive Self-Defence|QID|66554|M|66.23,12.77|N|From Sonova Snowden|US|
    ;C Hollow Up|QID|66244|M|39.49,63.06|N|From Sonova Snowden|US|
    T Aggressive Self-Defence|QID|66554|M|65.38,15.93|N|From Sonova Snowden
    ;T Hollow Up|QID|66244|M|65.38,15.93|N|From Sonova Snowden
-   A Bear with me|QID|66244|M|65.38,15.93|N|From Sonova Snowden
-   C Bear with me|QID|66244|M|65.38,15.93|N|Speak to Sonova Snowden
-   T Bear with me|QID|66244|M|65.38,15.93|N|To Sonova Snowden
+   A Bear with me|QID|66555|M|65.38,15.93|N|From Sonova Snowden
+   C Bear with me|QID|66555|M|65.38,15.93|N|Speak to Sonova Snowden
+   T Bear with me|QID|66555|M|65.38,15.93|N|To Sonova Snowden
    A Ice Cave Ya Got There|QID|66556|M|65.38,15.93|N|From Sonova Snowden
    C Ice Cave Ya Got There|QID|66556|QO|1|M|66.26,11.12|N|Enter the cave
    C Ice Cave Ya Got There|QID|66556|QO|2|M|66.05,10.83|N|Kill Rynea
    T Ice Cave Ya Got There|QID|66556|M|65.38,15.93|N|To Sonova Snowden
+   C To Valdrakken|QID|66244|M|39.49,63.06|N|Fly to the Valdrakken
    ]]
 end)

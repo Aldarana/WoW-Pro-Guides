3e7bacb4ed03aa6332be7b443b5e196052eebf22
jamiejme09 <113785394+jamiejme09@users.noreply.github.com>
2022-10-04 18:30:53 +0100

    Zone Thaldraszus

---
+++ WoWPro_Leveling/Neutral/DF_Thaldraszus.lua
@@ -0,0 +1,11 @@
+local guide = WoWPro:RegisterGuide("DF_T", "Leveling", "Thaldraszus", "Aldarana and Jme", "Neutral", 10)
+WoWPro:GuideName(guide,"Thaldraszus")
+WoWPro:GuideContent(guide, "Dragonflight")
+WoWPro:GuideLevels(guide, 62, 65)
+WoWPro:GuideSort(guide, 2)
+WoWPro:GuideSteps(guide, function()
+return [[
+  ;Chapter 1
+
+  ]]
+end)
